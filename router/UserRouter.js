dbd0d9b9f043fc54330a948a9f22716c28af50da
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-02-01 01:50:22 -0500

    docs: :memo: doc about the software
    database, reglas de negocio, http request and readme.md

--- router/UserRouter.js
+++ router/UserRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login,updateUsersPassword, updateUsersEmail, getUsers,createUsers,updateUsers,deleteUsers,getOneUser, me, logout} from '../controller/UserController.js';
+import { login,updateUsersPassword,getOneUser, me, logout} from '../controller/UserController.js';
 import  {verifyToken}  from '../middleware/auth.js';
 import { ValidateCreateUser } from '../middleware/validations/validateCreateUser.js';
 import { ValidateLoginUser } from '../middleware/validations/ValidateLoginUser.js';
@@ -10,10 +10,7 @@ rotuer.post('/register',ValidateCreateUser, createUsers);
 rotuer.post('/login', ValidateLoginUser,login);
 rotuer.get('/me', verifyToken,me);
 rotuer.get('/logout',verifyToken, logout);
-/* rotuer.get('/user',verifyToken, getUsers); */
 rotuer.get('/user/:id/reservations',verifyToken,verifyIsAdmin, getOneUser); 
-/*rotuer.put('/user/:id',verifyToken, updateUsers);
-rotuer.delete('/user/:id', verifyToken, deleteUsers); */
-/* rotuer.put('/user/email/:id',verifyToken, updateUsersEmail); */
+
 rotuer.put('/user/password',verifyToken,ValidateChangePasswordUser, updateUsersPassword);
 export const RouterUsuer = rotuer;
